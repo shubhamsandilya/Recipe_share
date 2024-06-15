a0689604bfd7152b89b00a4e836ee52c4bc9ca97
shubhamsandilya <shubhamsandilyadbg2@gmail.com>
2024-06-15 11:14:30 +0530

    bg image aded

--- Client/src/pages/AddForm.jsx
+++ Client/src/pages/AddForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import Form from "../assets/";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { useSelector } from "react-redux";
 // Importing a list of countries, you can find this list online or use a package
@@ -60,8 +61,14 @@ function AddForm() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <h1 className="text-3xl font-bold text-gray-900 mb-4">Add Recipe</h1>
+    <div
+      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage:
+          'url("https://t4.ftcdn.net/jpg/07/28/83/53/240_F_728835351_HVSeQ7fAZewLKLM5hDysk1kOQFZAQIXP.jpg")',
+      }}
+    >
+      <h1 className="text-3xl font-bold text-red-500 mb-4">Add Recipe</h1>
 
       <div className="max-w-2xl mx-auto p-4 bg-gray-900 text-white rounded-lg shadow-lg mb-5">
         <form onSubmit={handleSubmit}>
@@ -171,7 +178,7 @@ function AddForm() {
           <div className="flex justify-end">
             <button
               type="submit"
-              className="px-6 py-2 bg-indigo-500 text-white font-semibold rounded-md hover:bg-indigo-600 focus:outline-none"
+              className="px-6 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 focus:outline-none"
             >
               Submit
             </button>
